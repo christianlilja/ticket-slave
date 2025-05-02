f2a3aa3fd8d9a4b4ffcc79df448aaace54d0f062
Christian <crl@christianlilja.com>
2025-05-02 18:44:53 +0200

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, current_app, abort
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from datetime import datetime
 from functools import wraps
 from jinja2 import TemplateNotFound, TemplateSyntaxError
